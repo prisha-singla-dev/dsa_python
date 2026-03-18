e423b7266e89d6b9e41418af7c652ea183e68129
prisha-singla-dev <prishasingla23@gmail.com>
2026-03-18 11:03:31 +0530

    Valid sudoku

--- Week3/Day5/valid_sudoku.py
+++ Week3/Day5/valid_sudoku.py
@@ -1,4 +1,9 @@
 #valid sudoku
+# Brute Force Approach:
+# we can check each row, column, and 3x3 box for duplicates. This approach has a time complexity of O(9^2) = O(81) since we are checking 9 rows, 9 columns, and 9 boxes.
+# but this process becomes repetitive
+
+# optimized approach1:
 def isValidSudoku(board):
     rows = {}
     cols = {}
@@ -25,6 +30,35 @@ def isValidSudoku(board):
                 boxes[(box_index, num)] = True
 
     return True
+    # Time Complexity: O(1) - The board size is fixed (9x9), so the time complexity is constant.
+    # Space Complexity: O(1) - The space used for the hash maps is also
+    # constant since the board size is fixed.
+
+# optimal approach2:
+def isValidSudoku(board):
+    rows = [set() for _ in range(9)]
+    cols = [set() for _ in range(9)]
+    boxes = [set() for _ in range(9)]
+
+    for i in range(9):
+        for j in range(9):
+
+            val = board[i][j]
+
+            if val == ".":
+                continue
+
+            box_index = (i // 3) * 3 + (j // 3)
+
+            if val in rows[i] or val in cols[j] or val in boxes[box_index]:
+                return False
+
+            rows[i].add(val)
+            cols[j].add(val)
+            boxes[box_index].add(val)
+
+    return True
+    
 
 if __name__ == "__main__":
     board = [
